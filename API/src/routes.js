fe017afc8e0b4fef9c9469151c3c3dbc30832e81
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-01-02 19:44:21 -0300

    criando model de produto

--- API/src/routes.js
+++ API/src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import ProductController from './app/controllers/ProductController.js';
 import SessionController from './app/controllers/SessionController.js';
 import UserController from './app/controllers/UserController.js';
 
@@ -7,5 +8,6 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+routes.post('/products', ProductController.store);
 
 export default routes;
