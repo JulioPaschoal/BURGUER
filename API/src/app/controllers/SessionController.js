a519e2a962d01ade676a47bda572a49c60550d9f
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-01-02 09:48:28 -0300

    refatorando session de login

--- API/src/app/controllers/SessionController.js
+++ API/src/app/controllers/SessionController.js
@@ -7,27 +7,29 @@ class SessionController {
   async store(req, res) {
     const schema = Yup.object().shape({
       email: Yup.string().email().required(),
-      password: Yup.string().required(),
+      password: Yup.string().min(6).required(),
     });
-
     const isValid = await schema.isValid(req.body, { strict: true });
     if (!isValid) {
       return res.status(400).json({ error: 'Email or password is required' });
     }
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
+    const existsUser = await User.findOne({ where: { email } });
+    if (!existsUser) {
       return res.status(401).json({ error: 'Email or password is incorrect' });
     }
-    const passwordMatch = await bcrypt.compare(password, user.password_hash);
-    if (!passwordMatch) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existsUser.password_hash,
+    );
+    if (!isPasswordValid) {
       return res.status(401).json({ error: 'Email or password is incorrect' });
     }
     return res.status(200).json({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      admin: user.admin,
+      id: existsUser.id,
+      name: existsUser.name,
+      email: existsUser.email,
+      admin: existsUser.admin,
     });
   }
 }
