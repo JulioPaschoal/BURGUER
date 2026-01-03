0e31d7e4b42550cd22359a3850ccd92d224d2b5a
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-01-03 12:13:53 -0300

    middleware de autenticação

--- API/eslint.config.js
+++ API/eslint.config.js
@@ -18,6 +18,7 @@ export default defineConfig([
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'off',
     },
   },
 ]);
---
+++ API/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = async (req, res, next) => {
+  const authToken = req.headers.authorization;
+  if (!authToken) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+  const token = authToken.split(' ')[1];
+  try {
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) {
+        throw new Error();
+      }
+      req.userId = decoded.id;
+    });
+  } catch (error) {
+    return res.status(401).json({ error: 'Token is invalid' });
+  }
+  return next();
+};
+
+export default authMiddleware;
