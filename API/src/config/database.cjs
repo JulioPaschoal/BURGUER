d3154ae21f2c81d74ad532c98a45a6fe2c338582
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-01-08 22:47:34 -0300

    rodando a primeira migration

--- API/src/config/database.cjs
+++ API/src/config/database.cjs
@@ -2,10 +2,10 @@
 module.exports = {
   dialect: 'postgres',
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   define: {
     timestamps: true,
     underscored: true,
