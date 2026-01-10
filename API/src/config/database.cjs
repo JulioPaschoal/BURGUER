83492e47a9cbd3154bde3f9ce603960cc9302300
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-01-10 16:49:47 -0300

    refatorando database

--- API/src/config/database.cjs
+++ API/src/config/database.cjs
@@ -1,11 +1,11 @@
 // CONFIGURAÇÃO DO BANCO DE DADOS \\
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  port: '5432',
+  username: 'admin',
+  password: '@505619',
+  database: 'burguer',
   define: {
     timestamps: true,
     underscored: true,
