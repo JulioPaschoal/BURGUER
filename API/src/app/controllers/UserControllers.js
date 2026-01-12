d6baee3d0d750dfae0d35dd7e0f0abc0d59fca01
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-01-11 21:43:07 -0300

    validanado dados com yup

--- API/src/app/controllers/UserControllers.js
+++ API/src/app/controllers/UserControllers.js
@@ -1,11 +1,26 @@
 // CONFIGURAÇÃO DO CONTROLLER DE USUÁRIO \\
 import User from '../model/User.js';
 import { v4 } from 'uuid';
+import * as yup from 'yup';
 
 class UserControllers {
   async store(req, res) {
-    // RECEBENDO OS DADOS DO USUÁRIO \\
+    // VALIDANDO OS DADOS DO USUÁRIO \\
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password_hash: yup.string().min(6).required(),
+      admin: yup.boolean(),
+    });
+    try {
+      await schema.validateSync(req.body, { abortEarly: false, strict: true });
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
+    }
+
+    // RECEBENDO OS DADOS DO USUÁRIO VALIDADOS \\
     const { name, email, password_hash, admin } = req.body;
+
     // VERIFICANDO SE O E-MAIL JÁ EXISTE \\
     const userExists = await User.findOne({ where: { email } });
     if (userExists) {
