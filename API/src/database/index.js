af455aa55c090bbdd3639d15598e9c8a051dbfa7
JULIO PASCHOAL <juliocpaschoal@gmail.com>
2026-01-21 22:56:38 -0300

    configurando mongodb

--- API/src/database/index.js
+++ API/src/database/index.js
@@ -6,6 +6,7 @@ import User from '../app/model/User.js';
 import databaseConfig from '../config/database.cjs';
 import Product from '../app/model/Product.js';
 import Category from '../app/model/Category.js';
+import mongoose from 'mongoose';
 
 // PASSANDO AS CONFIGURAÇÕES DO BANCO DE DADOS \\
 const models = [User, Product, Category];
@@ -13,12 +14,21 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map((model) => model.init(this.connection))
-    .map(model => model.associate && model.associate(this.connection.models));
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models),
+      );
+  }
+  mongo() {
+    this.mongooseConnection = mongoose.connect(
+      'mongodb://localhost:27017/burguer',
+    );
   }
 }
 
